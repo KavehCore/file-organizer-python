0b4a534be85d74c8e89d7b848b0d52140b55fca9
ali <aaskarnia@gmail.com>
2026-09-13 15:31:46 +0330

    create base structure

---
+++ main.py
@@ -0,0 +1,17 @@
+# import modules and global variables
+
+
+
+# declare categories and extensions
+
+
+
+# create directories based on categories
+
+
+
+# searching and categorizing files
+
+
+
+# run the application
